990639c1097c93b981ceec5fec5bfd8963024c57
AnaVucic <anavucic007@gmail.com>
2023-03-12 23:45:33 +0100

    Napravljena nefunkcionalna klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,26 @@
+package magacin;
+
+import domain.Artikal;
+import interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajAtrikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
