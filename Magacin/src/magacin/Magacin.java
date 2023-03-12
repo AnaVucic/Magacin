000092b097cbd04655206771a397a0d9bb79b5ce
AnaVucic <anavucic007@gmail.com>
2023-03-12 23:52:15 +0100

    Implementirana klasa Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,25 +1,38 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import domain.Artikal;
 import interfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
+	
+	private List<Artikal> artikli = new ArrayList<Artikal>();
 
 	@Override
 	public void dodajAtrikal(Artikal artikal) {
-		// TODO Auto-generated method stub
-		
+		if (artikal == null)
+			throw new NullPointerException("Artikal koji se unosi ne sme biti null");
+		if (artikli.contains(artikal))
+			throw new IllegalArgumentException("Duplikat artikla ne moze biti ubacen");
+		artikli.add(artikal);		
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		artikli.remove(artikal);
 		
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		if(sifra < 0)
+			throw new IllegalArgumentException("Sifra za pronalazenje artikla ne moze biti negativna vrednost");
+		for(Artikal a: artikli) {
+			if(a.getSifra() == sifra)
+				return a;
+		}
 		return null;
 	}
 
