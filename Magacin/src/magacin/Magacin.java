dfaaa92992dfa97beb04c64054bc17137a56b5f8
AnaVucic <anavucic007@gmail.com>
2023-03-12 23:58:12 +0100

    Implementirana metoda dodajArtikal u klasi Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,13 +1,24 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import domain.Artikal;
 import interfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
+	
+	List<Artikal> artikli = new ArrayList<Artikal>();
 
 	@Override
 	public void dodajAtrikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if (artikal == null)
+			throw new NullPointerException("Artikal koji se unosi ne sme biti null");
+		for(Artikal a: artikli) {
+			if(a.getNaziv().equals(artikal.getNaziv()))
+				throw new IllegalArgumentException("Dva artikla ne smeju imati isti naziv");
+		}
+		artikli.add(artikal);
 		
 	}
 
