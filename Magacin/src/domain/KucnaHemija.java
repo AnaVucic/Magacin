87b31d3bd7e9ab6ea2ff6cddffbfcfbf4e8235a2
AnaVucic <anavucic007@gmail.com>
2023-03-12 23:12:01 +0100

    Izmenjene klase Artikal i KucnaHemija. Override-ovane metode toString, hashCode i equals

--- Magacin/src/domain/KucnaHemija.java
+++ Magacin/src/domain/KucnaHemija.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class KucnaHemija extends Artikal {
 
@@ -12,6 +13,31 @@ public class KucnaHemija extends Artikal {
 
 	public void setDatum(LocalDate datum) {
 		this.datum = datum;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(datum);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(datum, other.datum);
+	}
+
+	@Override
+	public String toString() {
+		return "KucnaHemija [datum=" + datum + "; " + super.toString() + "]";
 	}
 	
 	
